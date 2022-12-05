ff7217ff407c41770f3974880e9c07a0aa78d640
jillsiss <jilllsiss@gmail.com>
2022-12-05 14:19:47 -0600

    Quiz class added

--- LiftOffProject/Migrations/20221205201723_QuizClass.Designer.cs
+++ LiftOffProject/Migrations/20221205201723_QuizClass.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using LiftOffProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LiftOffProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221205201723_QuizClass")]
+    partial class QuizClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
