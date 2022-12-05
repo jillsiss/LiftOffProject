b936bf713c2aaaac738e8cc7b0056a2feabf783f
jillsiss <jilllsiss@gmail.com>
2022-12-05 15:22:12 -0600

    Add basic quiz functionality

---
+++ LiftOffProject/ViewModels/AddQuizViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using LiftOffProject.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LiftOffProject.ViewModels
+{
+    public class AddQuizViewModel
+    {
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        public DateTime Published { get; set; }
+        public AddQuizViewModel()
+        {
+
+        }
+    }
+}
