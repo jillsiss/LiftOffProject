4215c842c95377651fa8f2675665c1a74d3f044d
jillsiss <jilllsiss@gmail.com>
2022-12-05 16:25:09 -0600

    Added question and answer classes

---
+++ LiftOffProject/Models/Answers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace LiftOffProject.Models
+{
+    public class Answers
+    {
+        public int Id { get; set; }
+        public bool IsAnswer { get; set; }
+        public bool IsSelected { get; set; }
+        public Question Question { get; set; }
+        public string Choice { get; set; }
+
+        public Answers()
+        {
+        }
+    }
+}
--- LiftOffProject/Program.cs
+++ LiftOffProject/Program.cs
@@ -22,6 +22,8 @@ namespace LiftOffProject
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
     }
 }
 //Babies first push//
